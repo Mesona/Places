d68dec3dd95f4a40abf54d29b35c509e7b83f9b7
mesona <kylemesona@gmail.com>
2019-03-01 11:52:07 -0800

    some minor styling fixes to footer and header

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -8,8 +8,8 @@ class Footer extends React.Component {
       <footer className="footer">
         <section className="promo-links">
           {/* Github icon from https://www.flaticon.com/free-icon/github-logo_25231 */}
-          <p><a href="https://github.com/Mesona"><img src={window.images.githubImg}></img></a></p>
-          <p><a href="https://www.linkedin.com/in/kmfischer/"><img src={window.images.linkedinImg}></img></a></p>
+          <span><a href="https://github.com/Mesona"><img src={window.images.githubImg}></img></a></span>
+          <span><a href="https://www.linkedin.com/in/kmfischer/"><img src={window.images.linkedinImg}></img></a></span>
         </section>
         <section className="new-place">
           <button className="new-place-button"><img src={window.images.plusImg} className="plus-symbol"></img></button>
